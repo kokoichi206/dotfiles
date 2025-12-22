2d4c675520649af5c0e152d82c490729b1d6d6b3
TAKAHiRO TOMiNAGA <52474650+kokoichi206@users.noreply.github.com>
2025-12-23 05:40:09 +0900

    add nvim-treesitter

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -4,6 +4,18 @@ return {
 		require('kanagawa').setup({
 			compile=true,
 			transparent=true,
+			overrides=function(colors)
+				return {
+					-- Use :Inspect command
+					["@markup.link.url.markdown_inline"] = { link = "Special" }, -- (url)
+					["@markup.link.label.markdown_inline"] = { link = "WarningMsg" }, -- [label]
+					["@markup.italic.markdown_inline"] = { link = "Exception" }, -- *italic*
+					["@markup.raw.markdown_inline"] = { link = "String" }, -- `code`
+					["@markup.list.markdown"] = { link = "Function" }, -- + list
+					["@markup.quote.markdown"] = { link = "Error" }, -- > blockcode
+					["@markup.list.checked.markdown"] = { link = "WarningMsg" }, -- - [X] Checked
+				}
+			end
 		});
 		vim.cmd("colorscheme kanagawa");
 	end,
