87f1ab77b78ea0b2bfffc5c4aa3bc39cdfd7f67e
kokoichi206 <52474650+kokoichi206@users.noreply.github.com>
2026-02-26 17:25:27 +0900

    improve markdown rendering in nvim

---
+++ .config/nvim/lua/plugins/autolist.lua
@@ -0,0 +1,13 @@
+return {
+    "gaoDean/autolist.nvim",
+    ft = { "markdown", "text" },
+    opts = {},
+    keys = {
+        { "<CR>", "<CR><cmd>AutolistNewBullet<CR>", mode = "i", ft = { "markdown", "text" } },
+        { "<CR>", "<cmd>AutolistToggleCheckbox<CR>", mode = "n", ft = { "markdown", "text" } },
+        { "<Tab>", "<cmd>AutolistTab<CR>", mode = "i", ft = { "markdown", "text" } },
+        { "<S-Tab>", "<cmd>AutolistShiftTab<CR>", mode = "i", ft = { "markdown", "text" } },
+        { "o", "o<cmd>AutolistNewBullet<CR>", mode = "n", ft = { "markdown", "text" } },
+        { "O", "O<cmd>AutolistNewBulletBefore<CR>", mode = "n", ft = { "markdown", "text" } },
+    },
+}
