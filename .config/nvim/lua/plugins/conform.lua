e0900fe58dcd81c74d4ac8a5ef57ff0076a0378b
TAKAHiRO TOMiNAGA <52474650+kokoichi206@users.noreply.github.com>
2026-03-19 00:03:53 +0900

    より多くのファイルタイプで prettier をサポートし、ローカルフォーマッタを優先

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -10,6 +10,23 @@ return {
 			-- Conform will run the first available formatter
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 			typescript = { "prettierd", "prettier", stop_after_first = true },
+			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+			yaml = { "prettierd", "prettier", stop_after_first = true },
+			json = { "prettierd", "prettier", stop_after_first = true },
+			jsonc = { "prettierd", "prettier", stop_after_first = true },
+			css = { "prettierd", "prettier", stop_after_first = true },
+			html = { "prettierd", "prettier", stop_after_first = true },
+			markdown = { "prettierd", "prettier", stop_after_first = true },
+		},
+		formatters = {
+			-- プロジェクトローカルの prettierd/prettier を優先する
+			prettierd = {
+				require_cwd = true,
+			},
+			prettier = {
+				require_cwd = true,
+			},
 		},
 		format_on_save = {
 			lsp_format = "fallback",
