bc54c2e25c59a231e8ddf9440e7c69c485abdc3b
TAKAHiRO TOMiNAGA <52474650+kokoichi206@users.noreply.github.com>
2026-03-23 07:41:43 +0900

    nvim で ff 検索の対象を cwd から移動しない

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -2,9 +2,13 @@ return {
 	"ahmedkhalf/project.nvim",
 	init = function()
 		require("project_nvim").setup({
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+			-- manual_mode = true にすると、バッファを開いた時に自動で cwd を
+			-- git root に変更しなくなる。
+			-- デフォルト(false)では project.nvim が git root を検出して cwd を
+			-- 自動変更するため、fzf-lua の <leader>ff 等が nvim を起動した
+			-- ディレクトリではなくリポジトリルートを起点に検索してしまう。
+			-- 元の挙動に戻したい場合は manual_mode を false にするか、この行を削除する。
+			manual_mode = true,
 		})
 	end,
 }
